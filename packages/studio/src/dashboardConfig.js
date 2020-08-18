129b3ebd6451637c56ce5a446a8ccddf21e7e491
liberty <platformservices@gmail.com>
2020-08-18 06:14:54 +0000

    Initial Commit

--- packages/studio/src/dashboardConfig.js
+++ packages/studio/src/dashboardConfig.js
@@ -18,12 +18,12 @@ export default {
         data: [
           {
             title: 'GitHub repo',
-            value: 'REPO_URL',
+            value: 'https://github.com/purnomoeko/loveforall.git',
             category: 'Code'
           },
           {
             title: 'Frontend',
-            value: 'FRONTEND_URL',
+            value: 'http://serene-darwin-667edd.netlify.app',
             category: 'apps'
           }
         ]
